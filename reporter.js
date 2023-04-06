e5b9a015c8420e20189c2a584d7805cdd084fba4
Peter deHaan <peter@deseloper.com>
2023-04-06 15:54:48 -0700

    Add BreachDate to READMEs

--- reporter.js
+++ reporter.js
@@ -1,5 +1,17 @@
 import _groupBy from "lodash.groupby";
-import stats from "./stats-https.json" assert { type: "json" };
+// import stats from "./stats-https.json" assert { type: "json" };
+
+const PROTOCOL = process.env.PROTOCOL || "https";
+
+const { default: stats } = await import(`./stats-${PROTOCOL}.json`, { assert: { type: "json" }});
+
+const domainMap = await breachesByDomain();
+
+function getBreachDatesByDomain(domain) {
+  const breachDates = domainMap.get(domain).map(breach => new Intl.DateTimeFormat('en').format(new Date(breach.BreachDate)));
+  const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+  return formatter.format(breachDates);
+}
 
 const { true: success, false: errors } = _groupBy(stats.results, (r) => !("error" in r));
 
@@ -8,12 +20,14 @@ groupByErrors(errors);
 
 function groupByStatus(results = []) {
   const domainsByStatus = _groupBy(results, "status");
-  console.log(`\n\n# SUCCESS(ish) (${results.length})`);
+  console.log(`\n\n# ${PROTOCOL.toUpperCase()} SUCCESS(ish) (${results.length})`);
   for (const [code, domains] of Object.entries(domainsByStatus)) {
-    console.log(`- ${code} (${domains.length})`);
+    console.log("<details>");
+    console.log(`<summary>${code} (${domains.length})</summary>\n`);
     for (const res of domains) {
-      console.log(`  - [${res.status}/${res.statusText}] ${res.domain} -- ${res.url}`);
+      console.log(`  - [${res.status}/${res.statusText}] ${res.domain} -- ${res.url} (${getBreachDatesByDomain(res.domain)})`);
     }
+    console.log("</details>");
   }
 }
 
@@ -23,11 +37,30 @@ function groupByErrors(results=[]) {
     return res;
   });
   const errorsByCode = _groupBy(hasErrors, "error");
-  console.log(`\n\n# ERRORS (${results.length})`);
+  console.log(`\n\n# ${PROTOCOL.toUpperCase()} ERRORS (${results.length})`);
   for (const [code, domains] of Object.entries(errorsByCode)) {
-    console.log(`- ${code.replace(/^page.goto:/, "").trim()} (${domains.length})`);
+    console.log("<details>");
+    console.log(`<summary>${code.replace(/^page.goto:/, "").trim()} (${domains.length})</summary>\n`);
     for (const { domain } of domains) {
-      console.log(`  - ${domain}`);
+      console.log(`  - ${domain} (${getBreachDatesByDomain(domain)})`);
     }
+    console.log("</details>")
   }
+}
+
+async function getBreaches() {
+  const breaches = await fetch("https://haveibeenpwned.com/api/v3/breaches");
+  return breaches.json();
+}
+
+async function breachesByDomain() {
+  const breaches = await getBreaches();
+  return breaches.reduce((map, breach) => {
+    const key = breach.Domain;
+    if (key) {
+      const arr = map.get(key) || [];
+      map.set(key, arr.concat(breach));
+    }
+    return map;
+  }, new Map());
 }
